71ce0a6284c46da975e90f7841acf822d430f190
youqishi1 <132738955+youqishi1@users.noreply.github.com>
2024-09-07 11:02:59 +0800

    Update login_script2.py

--- login_script2.py
+++ login_script2.py
@@ -1,41 +1,40 @@
-name: Run Login Script  # 运行登录脚本
+name: Run Login Script
 
 on:
-  workflow_dispatch:  # 允许手动触发这个工作流
+  workflow_dispatch:
   schedule:
     - cron: "0 0 */3 * *"  # 每三天运行一次，可以根据需求调整时间
   push:
     branches:
-      - main  # 仅在 `main` 分支有代码推送时运行工作流
-
+      - main
 jobs:
   login:
-    runs-on: ubuntu-latest  # 使用最新版本的 Ubuntu 作为运行环境
+    runs-on: ubuntu-latest
 
     steps:
-      - name: Checkout 仓库代码  # 检出仓库代码
-        uses: actions/checkout@v2  # 使用 GitHub Actions 的 checkout 动作来拉取代码
+      - name: Checkout 仓库代码
+        uses: actions/checkout@v2
 
-      - name: 设置 Python 环境  # 设置 Python 环境
-        uses: actions/setup-python@v2  # 使用 GitHub Actions 的 setup-python 动作来设置 Python 版本
+      - name: 设置 Python 环境
+        uses: actions/setup-python@v2
         with:
           python-version: '3.x'  # 设置你希望使用的 Python 版本，建议使用稳定版本
 
-      - name: Create accounts.json from environment variable  # 从环境变量创建 accounts.json 文件
-        run: echo "$ACCOUNTS_JSON" > accounts.json  # 将环境变量 ACCOUNTS_JSON 的值写入到 accounts.json 文件
+      - name: Create accounts.json from environment variable
+        run: echo "$ACCOUNTS_JSON" > accounts.json
         env:
-            ACCOUNTS_JSON: ${{ secrets.ACCOUNTS_JSON }}  # 从 GitHub Secrets 中获取 ACCOUNTS_JSON 环境变量
+            ACCOUNTS_JSON: ${{ secrets.ACCOUNTS_JSON }}  # 从GitHub Secrets中获取环境变量
 
-      - name: 安装依赖  # 安装依赖
+      - name: 安装依赖
         run: |
-          python -m pip install --upgrade pip  # 升级 pip
-          pip install pyppeteer aiofiles requests  # 安装所需的 Python 包
-          pip install --upgrade pyppeteer  # 升级 pyppeteer
+          python -m pip install --upgrade pip
+          pip install pyppeteer aiofiles requests
+          pip install --upgrade pyppeteer
 
-      - name: 运行登录脚本  # 运行登录脚本
+      - name: 运行登录脚本
         env:
-          TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}  # 从 GitHub Secrets 中获取 TELEGRAM_BOT_TOKEN
-          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}  # 从 GitHub Secrets 中获取 TELEGRAM_CHAT_ID
-          ACCOUNTS_JSON: ${{ secrets.ACCOUNTS_JSON }}  # 从 GitHub Secrets 中获取 ACCOUNTS_JSON
+          TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}
+          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+          ACCOUNTS_JSON: ${{ secrets.ACCOUNTS_JSON }}
         run: |
-          python login_script.py  # 运行 login_script.py 脚本
+          python login_script.py
